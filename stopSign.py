314f48a08b77bbee1a9a5238cd12df0b0c281dd4
Diego Calvo <calvod@umich.edu>
2016-02-21 23:06:06 -0500

    read image works in python

--- stopSign.py
+++ stopSign.py
@@ -3,13 +3,18 @@ import cv2
 
 #diego
 #%--------------------READ IMAGE--------------------%
-inputPhoto = cv2.imread('/CVInput/stop.jpg')
-print inputPhoto
+inputPhoto = cv2.imread('CVInput/stop.jpg')
+cv2.imshow('image', inputPhoto)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 #outputPhotoObject = open('/CVOutput/stop.jpg', 'r+')
 
 
 
 #%--------------------RESIZE IMAGE--------------------%
+#make longest edge 400
+
+#crop the bottom 4th of the image
 
 
 #%--------------------THRESHOLD IMAGE--------------------%
