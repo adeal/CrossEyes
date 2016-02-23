9a3da77888a3eb6f2b057706cde589965c5bce92
Diego Calvo <calvod@umich.edu>
2016-02-23 12:50:37 -0500

    stopSign.py loops through CVInput, outputting all of its jpgs into CVOutput

--- stopSign.py
+++ stopSign.py
@@ -4,7 +4,18 @@ import cv2
 
 #diego
 #%--------------------READ IMAGE--------------------%
-inputPhoto = cv2.imread('CVInput/stop.jpg')
+
+import os
+whichPicWeAt = 0
+for fn in os.listdir('CVInput'):
+	if fn[-4:] == '.jpg':
+		whichPicWeAt += 1
+		if whichPicWeAt == 5:
+			print "this is the one"
+			print fn
+		inputPhoto = cv2.imread('CVInput/' + fn)
+		print "Swag"
+#inputPhoto = cv2.imread('CVInput/stop.jpg')
 
 # cv2.imshow('image', inputPhoto)
 # cv2.waitKey(0)
@@ -14,72 +25,73 @@ inputPhoto = cv2.imread('CVInput/stop.jpg')
 #make longest edge 400
 
 #get dimensions of image
-height, width, channels = inputPhoto.shape
-#print height, width
+		height, width, channels = inputPhoto.shape
+		#print height, width
 
-if height > width:
-	translationFactor = 400.0 / height
-else:
-	translationFactor = 400.0 / width
-#print translationFactor
+		if height > width:
+			translationFactor = 400.0 / height
+		else:
+			translationFactor = 400.0 / width
+		#print translationFactor
 
-#resize the image so the longest edge is 1200 pixels, keeping the same aspect ratio
-resizedInputPhoto = cv2.resize(inputPhoto,None,fx=translationFactor, fy=translationFactor, interpolation = cv2.INTER_CUBIC)
-# cv2.imshow('image', resizedInputPhoto)
-# cv2.waitKey(0)
+		#resize the image so the longest edge is 1200 pixels, keeping the same aspect ratio
+		resizedInputPhoto = cv2.resize(inputPhoto,None,fx=translationFactor, fy=translationFactor, interpolation = cv2.INTER_CUBIC)
+		# cv2.imshow('image', resizedInputPhoto)
+		# cv2.waitKey(0)
 
-#crop the bottom 4th of the image
-threeFourthsDown = resizedInputPhoto.shape[0] * (3.0 / 4)
-# print inputPhoto.shape[0]
-# print resizedInputPhoto.shape[0]
-print threeFourthsDown
-croppedInputPhoto = resizedInputPhoto[0:threeFourthsDown, :]
-# cv2.imshow('image', croppedInputPhoto)
-# cv2.waitKey(0)
-
-
-#%--------------------THRESHOLD IMAGE--------------------%
-
-#go through each pixel
-for i in range(0, croppedInputPhoto.shape[0]):
-	for j in range(0, croppedInputPhoto.shape[1]):
-		#look at the pixel
-		colorOfPixel = croppedInputPhoto[i, j]
-		blue = colorOfPixel[0] + 0.0;
-		green = colorOfPixel[1] + 0.0;
-		red = colorOfPixel[2] + 0.0;
-		#print blue, green, red
-
-		#make sure we get no divide by zero errors
-		if blue == 0.0:
-			blue = 1
-		if green == 0.0:
-			green = 1
-
-		#if pixel is red enough, make it white
-		
-		if red / (blue + green) >= 1.1:
-			croppedInputPhoto[i, j] = [255, 255, 255]
-		else: #else, make it black
-			croppedInputPhoto[i, j] = [0, 0, 0]
+		#crop the bottom 4th of the image
+		threeFourthsDown = resizedInputPhoto.shape[0] * (3.0 / 4)
+		# print inputPhoto.shape[0]
+		# print resizedInputPhoto.shape[0]
+		print threeFourthsDown
+		croppedInputPhoto = resizedInputPhoto[0:threeFourthsDown, :]
+		# cv2.imshow('image', croppedInputPhoto)
+		# cv2.waitKey(0)
 
 
-biggerYetBlurrierThresholdedImageForViewing = cv2.resize(croppedInputPhoto,None,fx=(1 / translationFactor), fy=(1 / translationFactor), interpolation = cv2.INTER_CUBIC)
-height, width, channels = biggerYetBlurrierThresholdedImageForViewing.shape
-print height, width
-print translationFactor
-print 1 / translationFactor
-cv2.imwrite( "CVOutput/thresholdedImage.jpg", croppedInputPhoto)
-cv2.imshow('image', biggerYetBlurrierThresholdedImageForViewing)
-cv2.waitKey(0)		
+		#%--------------------THRESHOLD IMAGE--------------------%
+
+		#go through each pixel
+		for i in range(0, croppedInputPhoto.shape[0]):
+			for j in range(0, croppedInputPhoto.shape[1]):
+				#look at the pixel
+				colorOfPixel = croppedInputPhoto[i, j]
+				blue = colorOfPixel[0] + 0.0;
+				green = colorOfPixel[1] + 0.0;
+				red = colorOfPixel[2] + 0.0;
+				#print blue, green, red
+
+				#make sure we get no divide by zero errors
+				if blue == 0.0:
+					blue = 1
+				if green == 0.0:
+					green = 1
+
+				#if pixel is red enough, make it white
+				
+				if red / (blue + green) >= 1.1:
+					croppedInputPhoto[i, j] = [255, 255, 255]
+				else: #else, make it black
+					croppedInputPhoto[i, j] = [0, 0, 0]
+
+
+		biggerYetBlurrierThresholdedImageForViewing = cv2.resize(croppedInputPhoto,None,fx=(1 / translationFactor), fy=(1 / translationFactor), interpolation = cv2.INTER_CUBIC)
+		height, width, channels = biggerYetBlurrierThresholdedImageForViewing.shape
+		print height, width
+		print translationFactor
+		print 1 / translationFactor
+		stringWhichPicWeAt = str(whichPicWeAt)
+		cv2.imwrite( "CVOutput/thresholdedImage" + stringWhichPicWeAt + ".jpg", croppedInputPhoto)
+	# cv2.imshow('image', biggerYetBlurrierThresholdedImageForViewing)
+	# cv2.waitKey(0)		
 
 
 #austin
 #for this part, use the Thresholded_Images
 #%--------------------DETECT BLOBS--------------------%
-blobMeasurement = cv2.findContours(croppedInputPhoto, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-numberOfBlobs = len(blobMeasurement)
-print numberOfBlobs
+# blobMeasurement = cv2.findContours(croppedInputPhoto, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+# numberOfBlobs = len(blobMeasurement)
+# print numberOfBlobs
 
 
 
