123ddc080c5a0c5f5235679100a4e8a2491b8769
Diego Calvo <calvod@umich.edu>
2016-03-20 12:57:53 -0400

    can now detect crosswalk signs

--- crosswalkSign.py
+++ crosswalkSign.py
@@ -8,14 +8,10 @@ import cv2
 
 import os
 whichPicWeAt = 0
-for fn in os.listdir('CVInput'):
+for fn in os.listdir('CrossWalkSignInput'):
 	if fn[-4:] == '.jpg':
 		whichPicWeAt += 1
-		if whichPicWeAt == 5:
-			print "this is the one"
-			print fn
-		inputPhoto = cv2.imread('CVInput/' + fn)
-		print "Swag"
+		inputPhoto = cv2.imread('CrossWalkSignInput/' + fn)
 #inputPhoto = cv2.imread('CVInput/stop.jpg')
 
 # cv2.imshow('image', inputPhoto)
@@ -44,7 +40,7 @@ for fn in os.listdir('CVInput'):
 		threeFourthsDown = resizedInputPhoto.shape[0] * (3.0 / 4)
 		# print inputPhoto.shape[0]
 		# print resizedInputPhoto.shape[0]
-		print threeFourthsDown
+		#print threeFourthsDown
 		croppedInputPhoto = resizedInputPhoto[0:threeFourthsDown, :]
 		# cv2.imshow('image', croppedInputPhoto)
 		# cv2.waitKey(0)
@@ -67,10 +63,12 @@ for fn in os.listdir('CVInput'):
 					blue = 1
 				if green == 0.0:
 					green = 1
+				if red == 0.0:
+					red = 1
 
-				#if pixel is red enough, make it white
+				#if pixel is yellow enough, make it white
 				
-				if red / (blue + green) >= 1.1:
+				if red > 230 and green > 180 and green < 224 and blue < 15:
 					croppedInputPhoto[i, j] = [255, 255, 255]
 				else: #else, make it black
 					croppedInputPhoto[i, j] = [0, 0, 0]
@@ -78,11 +76,11 @@ for fn in os.listdir('CVInput'):
 
 		biggerYetBlurrierThresholdedImageForViewing = cv2.resize(croppedInputPhoto,None,fx=(1 / translationFactor), fy=(1 / translationFactor), interpolation = cv2.INTER_CUBIC)
 		height, width, channels = biggerYetBlurrierThresholdedImageForViewing.shape
-		print height, width
-		print translationFactor
-		print 1 / translationFactor
+		# print height, width
+		# print translationFactor
+		# print 1 / translationFactor
 		stringWhichPicWeAt = str(whichPicWeAt)
-		cv2.imwrite( "CVOutput/thresholdedImage" + stringWhichPicWeAt + ".jpg", croppedInputPhoto)
+		cv2.imwrite( "CrossWalkSignOutput/thresholdedImage" + stringWhichPicWeAt + ".jpg", croppedInputPhoto)
 	# cv2.imshow('image', biggerYetBlurrierThresholdedImageForViewing)
 	# cv2.waitKey(0)		
 
@@ -101,22 +99,28 @@ for fn in os.listdir('CVInput'):
 		# cv2.imshow('img', img)
 		# cv2.waitKey(0)
 
-		numberOfContours = len(contours)
-		area = cv2.contourArea(contours[0], False)
-		# print area
 
-		largestArea = 0
-		largestContour = 0
-		for i in range(0, numberOfContours):
-		    area = cv2.contourArea(contours[i], False)
-		    if area > largestArea:
-		        largestArea = area
-		        largestContour = i
+		#don't error if there aren't any contours
+		if len(contours) > 0:
+			numberOfContours = len(contours)
+			area = cv2.contourArea(contours[0], False)
+			# print area
 
-		cv2.drawContours(resizedInputPhoto, contours, largestContour, (0,255,0), 3)
-		cv2.imshow('contours', resizedInputPhoto)
+			largestArea = 0
+			largestContour = 0
+			for i in range(0, numberOfContours):
+			    area = cv2.contourArea(contours[i], False)
+			    if area > largestArea:
+			        largestArea = area
+			        largestContour = i
 
-		cv2.waitKey(0)
+			cv2.drawContours(resizedInputPhoto, contours, largestContour, (0,255,0), 3)
+			cv2.imshow('contours', resizedInputPhoto)
+
+			cv2.waitKey(0)
+		else:
+			print "No contours found in this image"
+			print fn
 
 
 		# thresholdedInputPhoto = []
