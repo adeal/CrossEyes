94a3da0b450d10892080a89215f9123adf454e33
Diego Calvo <calvod@umich.edu>
2016-04-08 13:24:58 -0400

    testing crosswalk lines false positives

--- crosswalkLines.py
+++ crosswalkLines.py
@@ -3,6 +3,8 @@ import cv2
 import heapq
 import os
 
+translationFactorCalibration = 400.0
+
 
 class streamMedian:
     def __init__(self):
@@ -33,88 +35,90 @@ class streamMedian:
 
 # while True:
 for fn in os.listdir('linesOnly'):
-    print "\nProcessing new image..."
-    inputPhoto = cv2.imread('linesOnly/' + fn)
-    cv2.imshow('Input Photo', inputPhoto)
-    cv2.moveWindow('Input Photo', 0, 0)
-    cv2.waitKey(0)
+    if fn[-4:] == '.jpg' or fn[-4:] == '.JPG':
+        print "\nProcessing new image..."
+        print "image name is " + fn
+        inputPhoto = cv2.imread('linesOnly/' + fn)
+        cv2.imshow('Input Photo', inputPhoto)
+        cv2.moveWindow('Input Photo', 0, 0)
+        cv2.waitKey(0)
 
-    cv2.destroyWindow('Input Photo')
+        cv2.destroyWindow('Input Photo')
 
-# inputPhoto = cv2.imread('roadFiles/20160316_142627.jpg')
+    # inputPhoto = cv2.imread('roadFiles/20160316_142627.jpg')
 
-    height, width, channels = inputPhoto.shape
-    # print height, width
+        height, width, channels = inputPhoto.shape
+        # print height, width
 
-    if height > width:
-        translationFactor = 400.0 / height
-    else:
-        translationFactor = 400.0 / width
-    # print translationFactor
+        if height > width:
+            translationFactor = translationFactorCalibration / height
+        else:
+            translationFactor = translationFactorCalibration / width
+        # print translationFactor
 
-    # resize the image so the longest edge is 1200 pixels, keeping the same aspect ratio
-    resizedInputPhoto = cv2.resize(inputPhoto, None, fx=translationFactor, fy=translationFactor, interpolation=cv2.INTER_CUBIC)
-    height = resizedInputPhoto.shape[0]
-    # print inputPhoto.shape[0]
-    # print resizedInputPhoto.shape[0]
-    # print threeFourthsDown
-    # croppedInputPhoto = resizedInputPhoto[height / 4:height, :]
-    cv2.imshow('Cropped Image', resizedInputPhoto)
-    cv2.moveWindow('Cropped Image', 0, 0)
-    cv2.waitKey(0)
+        # resize the image so the longest edge is 1200 pixels, keeping the same aspect ratio
+        resizedInputPhoto = cv2.resize(inputPhoto, None, fx=translationFactor, fy=translationFactor, interpolation=cv2.INTER_CUBIC)
+        height = resizedInputPhoto.shape[0]
+        # print inputPhoto.shape[0]
+        # print resizedInputPhoto.shape[0]
+        # print threeFourthsDown
+        # croppedInputPhoto = resizedInputPhoto[height / 4:height, :]
+        cv2.imshow('Cropped Image', resizedInputPhoto)
+        cv2.moveWindow('Cropped Image', 0, 0)
+        cv2.waitKey(0)
 
-    gray_img = cv2.cvtColor(resizedInputPhoto, cv2.COLOR_BGR2GRAY)
-    # cv2.imshow('gray', gray)
-    # cv2.waitKey(0)
-    ret, gray_img = cv2.threshold(gray_img, 165, 255, cv2.THRESH_BINARY)
-    cv2.imshow('Thresholded Image', gray_img)
-    cv2.moveWindow('Thresholded Image', 0, 0)
-    cv2.waitKey(0)
-    # gray2 = gray.copy()
-    # mask = np.zeros(gray.shape,np.uint8)
+        gray_img = cv2.cvtColor(resizedInputPhoto, cv2.COLOR_BGR2GRAY)
+        # cv2.imshow('gray', gray)
+        # cv2.waitKey(0)
+        ret, gray_img = cv2.threshold(gray_img, 165, 255, cv2.THRESH_BINARY)
+        cv2.imshow('Thresholded Image', gray_img)
+        cv2.moveWindow('Thresholded Image', 0, 0)
+        cv2.waitKey(0)
+        # gray2 = gray.copy()
+        # mask = np.zeros(gray.shape,np.uint8)
 
-    img, contours, _ = cv2.findContours(gray_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # cv2.imshow('img', img)
-    # cv2.waitKey(3000)
+        img, contours, _ = cv2.findContours(gray_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        # cv2.imshow('img', img)
+        # cv2.waitKey(3000)
 
-    numContours = len(contours)
-    possibleLines = {}
-    midptMedian = streamMedian()
-    if numContours > 0:
-        for i in range(0, numContours):
-            area = cv2.contourArea(contours[i], False)
-            # print "area of this contour is " + str(area)
-            # make sure the contour is large enough to even bother with
-            if area > 100:
-                x, y, w, h = cv2.boundingRect(contours[i])
-                midPt = (x + x + w) / 2
-                # cv2.line(croppedInputPhoto, (midPt, 0), (midPt, height), (0,255,0), 1)
+        numContours = len(contours)
+        possibleLines = {}
+        midptMedian = streamMedian()
+        if numContours > 0:
+            for i in range(0, numContours):
+                area = cv2.contourArea(contours[i], False)
+                # print "area of this contour is " + str(area)
+                # make sure the contour is large enough to even bother with
+                if area > 100:
+                    x, y, w, h = cv2.boundingRect(contours[i])
+                    midPt = (x + x + w) / 2
+                    # cv2.line(croppedInputPhoto, (midPt, 0), (midPt, height), (0,255,0), 1)
 
-                midptMedian.insert(midPt)
-                possibleLines[i] = [x, y, w, h]
+                    midptMedian.insert(midPt)
+                    possibleLines[i] = [x, y, w, h]
 
-                # cv2.drawContours(croppedInputPhoto, contours, i, (0,255,0), 3)
-                # cv2.imshow('contours', croppedInputPhoto)
-                # cv2.waitKey(0)
+                    # cv2.drawContours(croppedInputPhoto, contours, i, (0,255,0), 3)
+                    # cv2.imshow('contours', croppedInputPhoto)
+                    # cv2.waitKey(0)
 
-    crossLineContourIndexes = []
-    median = midptMedian.getMedian()
-    # print "ok, done. median is: ", median
-    for key, val in possibleLines.iteritems():
-        x = val[0]
-        w = val[2]
+        crossLineContourIndexes = []
+        median = midptMedian.getMedian()
+        # print "ok, done. median is: ", median
+        for key, val in possibleLines.iteritems():
+            x = val[0]
+            w = val[2]
 
-        midPt = (x + x + w) / 2
-        # print "MIDPT for {} is {}".format(key, midPt)
-        if ((median - 40) <= midPt <= (median + 40)):
-            crossLineContourIndexes.append(key)
+            midPt = (x + x + w) / 2
+            # print "MIDPT for {} is {}".format(key, midPt)
+            if ((median - (translationFactorCalibration / 10)) <= midPt <= (median + (translationFactorCalibration / 10))):
+                crossLineContourIndexes.append(key)
 
-    if len(crossLineContourIndexes) >= 3:
-        print "CROSSWALK DETECTED"
-        for contour in crossLineContourIndexes:
-            cv2.drawContours(resizedInputPhoto, contours, contour, (0, 255, 0), 3)
+        if len(crossLineContourIndexes) >= 3:
+            print "CROSSWALK DETECTED"
+            for contour in crossLineContourIndexes:
+                cv2.drawContours(resizedInputPhoto, contours, contour, (0, 255, 0), 3)
 
-    cv2.imshow('FINAL IMAGE', resizedInputPhoto)
-    cv2.moveWindow('FINAL IMAGE', 0, 0)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+        cv2.imshow('FINAL IMAGE', resizedInputPhoto)
+        cv2.moveWindow('FINAL IMAGE', 0, 0)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
